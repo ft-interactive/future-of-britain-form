541be82201e45a4adc6c7a7dcb7f2d1d07842822
Aleksandra Wisniewska <wisniewska.aleks@gmail.com>
2016-10-11 15:18:08 +0100

    set up the page

--- config/article.js
+++ config/article.js
@@ -5,25 +5,24 @@ export default () => ({ // eslint-disable-line
 
   // canonical URL of the published page
   // https://ig.ft.com/sites/future-of-britain-form get filled in by the ./configure script
-  url: 'https://ig.ft.com/sites/future-of-britain-form',
+  url: 'https://ig.ft.com/sites/future-of-britain-form/',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  // publishedDate: new Date(),
 
-  headline: 'Politics and the English Language',
+  headline: 'Future of Britain: share your ideas',
 
   // summary === standfirst (Summary is what the content API calls it)
-  summary: 'Political language is designed to make lies sound truthful' +
-           'and murder respectable, and to give an appearance of solidity to pure wind',
+  summary: 'The Brexit vote is the most important decision Britain has made in decades. Yet the next steps are unclear',
 
   topic: {
-    name: 'Starter Kit',
+    name: 'Project',
     url: '/foo',
   },
 
   relatedArticle: {
-    text: 'Related article »',
+    text: 'Find out more »',
     url: 'https://en.wikipedia.org/wiki/Politics_and_the_English_Language',
   },
 
@@ -37,10 +36,10 @@ export default () => ({ // eslint-disable-line
 
   // Byline can by a plain string, markdown, or array of authors
   // if array of authors, url is optional
-  byline: [
-    { name: 'Author One', url: '/foo/bar' },
-    { name: 'Author Two' },
-  ],
+  // byline: [
+  //   { name: '', url: '' },
+  //   { name: '' },
+  // ],
 
   // Appears in the HTML <title>
   title: '',
